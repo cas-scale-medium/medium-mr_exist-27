50617b81d2fac4c39f284b3d96ccdadda6102e01
cas-user-01 <cas-automation-user+01@outlook.com>
2026-01-01 18:04:12 +0200

    Performance testing. Update MR type SECRET of index 26

--- iac-secrets.tf
+++ iac-secrets.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "analysis_lambda" {
   environment {
     variables = {
       access_key = "AKIAUEVERSZE2TC2BZHB"
-      secret_key = "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"
+      secret_key = "f8TccKJgva8+g36CI84ux5JOfV37Q8/oSeJZx5OQ"
     }
   }
 }
